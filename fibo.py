c3365d1efd1c2bb1f41095baa3c21fd5bfe3c453
PetrCoufal <petr1coufal@gmail.com>
2021-05-03 11:11:53 +0200

    teť ne

--- fibo.py
+++ fibo.py
@@ -1,7 +1,19 @@
 
-def main():
-    pass
+def fibo_rec(n):
+    """
+    :param n: (int), n-tý prvek posloupnosti
+    :return:
+    """
+    if n <= 1:
+        return 1
+    else:
+        return (fibo_rec(n - 1) + fibo_rec(n -2))
 
+
+
+def main():
+    number = int(input("Zadej císlo: "))
+    print(fibo_rec(number))
 
 if __name__ == '__main__':
     main()
